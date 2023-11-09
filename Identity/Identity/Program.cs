006a17d90999f1a462e37c190958f74251ec179f
YaroslavKSE <ikhomych@kse.org.ua>
2023-11-09 14:53:48 +0200

    Added GoogleOAuth endpoints in Program.cs. TODO Implement creating the user in DB

--- Identity/Identity/Program.cs
+++ Identity/Identity/Program.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Authentication.Google;
 using Identity.Core;
-// View Layer
+using Microsoft.AspNetCore.Authentication;
 
+// View Layer
 
 // Infrastructure As a Code
 
-
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+  {
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "Google";
+  })
+  .AddCookie()
+  .AddGoogle("Google", options =>
+  {
+    options.ClientId = "989838419075-cmepplaro69du10sdlftsr5f8u58gj7p.apps.googleusercontent.com";
+    options.ClientSecret = "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";
+  });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +28,9 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+// app.UseAuthentication();
+// app.UseAuthorization();
 
 var service = new UsersService();
 
@@ -25,5 +41,26 @@ app.MapPost("/user", async (UserDto user) =>
 {
   await service.AddAsync(user);
 });
+
+app.MapGet("/login", async context =>
+{
+  await context.ChallengeAsync("Google", new AuthenticationProperties { RedirectUri = "/" });
+});
+
+app.MapGet("/signin-google", async context =>
+{
+  var result = await context.AuthenticateAsync("Google");
+  if (result.Principal != null)
+  {
+    context.Response.Redirect("/");
+    // Create a user in your database if it doesn't exist
+    // Redirect to the original protected resource or a default page
+  }
+  else
+  {
+    context.Response.Redirect("/login-failed");
+  }
+});
+
 
 app.Run();
