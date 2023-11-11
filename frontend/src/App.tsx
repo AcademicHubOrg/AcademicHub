0c755b4ba09e11d90818368b37329d68ca0394fb
Yan_Khadnevich <90506265+ykhadnevich@users.noreply.github.com>
2023-11-11 20:11:02 +0200

    Did add course frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import CoursesListPage from './pages/CoursesListPage/CoursesListPage';
 import AuthPage from './pages/AuthPage/AuthPage';
+import AddCoursePage from './pages/AddCoursePage/AddCoursePage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/courselist" element={<CoursesListPage />} />
           <Route path="/auth" element={<AuthPage />} />
+            <Route path="/addcourse" element={<AddCoursePage />} />
         </Routes>
       </Router>
   );
