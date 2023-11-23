e24c1aac284df8b0c289ffc500e450bed305a2d2
Myroslav Kryvoruchko <90711690+kmirik999@users.noreply.github.com>
2023-11-24 00:27:43 +0200

    Code duplication reduced, Use data providers

--- CourseTemplate/CourseTemplate.UnitTests/UnitTest1.cs
+++ CourseTemplate/CourseTemplate.UnitTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using CourseTemplate.Core;
 using CourseTemplate.Data;
 using CustomExceptions;
@@ -5,100 +6,110 @@ using Moq;
 
 namespace CourseTemplate.UnitTests
 {
-	public class UnitTest1
-	{
-		[Fact]
-		public async Task AddAsync_WhenCourseDoesNotExist_ShouldAddCourse()
-		{
-			// Arrange
-			var mockRepository = new Mock<ICourseTemplateRepository>();
-			mockRepository.Setup(repo => repo.ListAsync())
-				.ReturnsAsync(new List<Data.CourseTemplate>());
+    public class UnitTest1
+    {
+        private readonly Mock<ICourseTemplateRepository> _mockRepository;
+        private readonly CourseTemplateService _service;
 
-			var service = new CourseTemplateService(mockRepository.Object);
-			var createCourse = new CreateCourseTemplateDto { Name = "NewCourse" };
+        public UnitTest1()
+        {
+            _mockRepository = new Mock<ICourseTemplateRepository>();
+            _service = new CourseTemplateService(_mockRepository.Object);
+        }
 
-			// Act
-			await service.AddAsync(createCourse);
+        [Fact]
+        public async Task AddAsync_WhenCourseDoesNotExist_ShouldAddCourse()
+        {
+            // Arrange
+            _mockRepository.Setup(repo => repo.ListAsync())
+                .ReturnsAsync(new List<Data.CourseTemplate>());
 
-			// Assert
-			mockRepository.Verify(repo => repo.AddAsync(It.IsAny<Data.CourseTemplate>()), Times.Once);
-		}
+            var createCourse = new CreateCourseTemplateDto { Name = "NewCourse" };
 
-		[Fact]
-		public async Task AddAsync_WhenCourseExists_ShouldThrowConflictException()
-		{
-			// Arrange
-			var existingCourse = new Data.CourseTemplate { Id = 1, CourseName = "ExistingCourse" };
-			var mockRepository = new Mock<ICourseTemplateRepository>();
-			mockRepository.Setup(repo => repo.ListAsync())
-				.ReturnsAsync(new List<Data.CourseTemplate> { existingCourse });
+            // Act
+            await _service.AddAsync(createCourse);
 
-			var service = new CourseTemplateService(mockRepository.Object);
-			var createCourse = new CreateCourseTemplateDto { Name = "ExistingCourse" };
+            // Assert
+            _mockRepository.Verify(repo => repo.AddAsync(It.IsAny<Data.CourseTemplate>()), Times.Once);
+        }
 
-			// Act and Assert
-			await Assert.ThrowsAsync<ConflictException>(() => service.AddAsync(createCourse));
-		}
-		
-		[Fact]
-		public async Task ListAsync_ShouldReturnListOfCourseTemplates()
-		{
-			// Arrange
-			var expectedCourseTemplates = new List<Data.CourseTemplate>
-			{
-				new Data.CourseTemplate { Id = 1, CourseName = "Course1" },
-				new Data.CourseTemplate { Id = 2, CourseName = "Course2" },
-				new Data.CourseTemplate { Id = 3, CourseName = "Course3" }
-			};
+        [Fact]
+        public async Task AddAsync_WhenCourseExists_ShouldThrowConflictException()
+        {
+            // Arrange
+            var existingCourse = new Data.CourseTemplate { Id = 1, CourseName = "ExistingCourse" };
+            _mockRepository.Setup(repo => repo.ListAsync())
+                .ReturnsAsync(new List<Data.CourseTemplate> { existingCourse });
 
-			var mockRepository = new Mock<ICourseTemplateRepository>();
-			mockRepository.Setup(repo => repo.ListAsync())
-				.ReturnsAsync(expectedCourseTemplates);
+            var createCourse = new CreateCourseTemplateDto { Name = "ExistingCourse" };
 
-			var service = new CourseTemplateService(mockRepository.Object);
+            // Act and Assert
+            await Assert.ThrowsAsync<ConflictException>(() => _service.AddAsync(createCourse));
+        }
 
-			// Act
-			var result = await service.ListAsync();
+        [Theory]
+        [ClassData(typeof(CourseTemplateTestData))]
+        public async Task ListAsync_ShouldReturnListOfCourseTemplates(List<Data.CourseTemplate> expectedCourseTemplates)
+        {
+            // Arrange
+            _mockRepository.Setup(repo => repo.ListAsync())
+                .ReturnsAsync(expectedCourseTemplates);
 
-			// Assert
-			Assert.Equal(expectedCourseTemplates.Count, result.Count);
-			Assert.Equal(expectedCourseTemplates.Select(c => c.Id), result.Select(c => int.Parse(c.Id)));
-			Assert.Equal(expectedCourseTemplates.Select(c => c.CourseName), result.Select(c => c.Name));
-		}
-		
-		[Fact]
-		public async Task GetByIdAsync_WhenCourseExists_ShouldReturnViewCourseTemplateDto()
-		{
-			// Arrange
-			var existingCourse = new Data.CourseTemplate { Id = 1, CourseName = "ExistingCourse" };
-			var mockRepository = new Mock<ICourseTemplateRepository>();
-			mockRepository.Setup(repo => repo.GetByIdAsync(1))
-				.ReturnsAsync(existingCourse);
+            // Act
+            var result = await _service.ListAsync();
 
-			var service = new CourseTemplateService(mockRepository.Object);
+            // Assert
+            Assert.Equal(expectedCourseTemplates.Count, result.Count);
+            Assert.Equal(expectedCourseTemplates.Select(c => c.Id), result.Select(c => int.Parse(c.Id)));
+            Assert.Equal(expectedCourseTemplates.Select(c => c.CourseName), result.Select(c => c.Name));
+        }
 
-			// Act
-			var result = await service.GetByIdAsync(1);
+        [Fact]
+        public async Task GetByIdAsync_WhenCourseExists_ShouldReturnViewCourseTemplateDto()
+        {
+            // Arrange
+            var existingCourse = new Data.CourseTemplate { Id = 1, CourseName = "ExistingCourse" };
+            _mockRepository.Setup(repo => repo.GetByIdAsync(1))
+                .ReturnsAsync(existingCourse);
 
-			// Assert
-			Assert.NotNull(result);
-			Assert.Equal(existingCourse.Id.ToString(), result.Id);
-			Assert.Equal(existingCourse.CourseName, result.Name);
-		}
-		
-		[Fact]
-		public async Task GetByIdAsync_WhenCourseDoesNotExist_ShouldThrowNotFoundException()
-		{
-			// Arrange
-			var mockRepository = new Mock<ICourseTemplateRepository>();
-			mockRepository.Setup(repo => repo.GetByIdAsync(1))
-				.ReturnsAsync((Data.CourseTemplate)null);
+            // Act
+            var result = await _service.GetByIdAsync(1);
 
-			var service = new CourseTemplateService(mockRepository.Object);
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(existingCourse.Id.ToString(), result.Id);
+            Assert.Equal(existingCourse.CourseName, result.Name);
+        }
 
-			// Act and Assert
-			await Assert.ThrowsAsync<NotFoundException>(() => service.GetByIdAsync(1));
-		}
-	}
+        [Fact]
+        public async Task GetByIdAsync_WhenCourseDoesNotExist_ShouldThrowNotFoundException()
+        {
+            // Arrange
+            _mockRepository.Setup(repo => repo.GetByIdAsync(1))
+                .ReturnsAsync((Data.CourseTemplate)null);
+
+            // Act and Assert
+            await Assert.ThrowsAsync<NotFoundException>(() => _service.GetByIdAsync(1));
+        }
+    }
+
+    public class CourseTemplateTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new List<Data.CourseTemplate>
+                {
+                    new Data.CourseTemplate { Id = 1, CourseName = "Course1" },
+                    new Data.CourseTemplate { Id = 2, CourseName = "Course2" },
+                    new Data.CourseTemplate { Id = 3, CourseName = "Course3" }
+                }
+            };
+
+            // Add more test cases as needed
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
 }
