ba9225fbd70414c568dd40f5c3cfe662baf78ce6
YaroslavKSE <ikhomych@kse.org.ua>
2023-11-29 13:12:36 +0200

    Update script to check Data layer coverage-check.sh. Updated tests yml in order to start them only on changes to specific microservice

--- scripts/coverage-check.sh
+++ scripts/coverage-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the threshold
-THRESHOLD=80
+THRESHOLD=95
 
 # Find the coverage report
 COBERTURA_REPORT=$(find . -name "coverage.cobertura.xml" | head -n 1)
@@ -11,13 +11,19 @@ if [ -z "$COBERTURA_REPORT" ]; then
   exit 1
 fi
 
-# Extract overall coverage percentage
-COVERAGE_PERCENTAGE=$(grep 'line-rate' "$COBERTURA_REPORT" | head -n 1 | sed -E 's/.*line-rate="([0-9.]+)".*/\1/' | awk '{print $1 * 100}')
+# Extract Identity.Core branch coverage percentage
+IDENTITY_CORE_BRANCH_COVERAGE=$(grep 'Identity.Core' "$COBERTURA_REPORT" | grep 'branch-rate' | sed -E 's/.*branch-rate="([0-9.]+)".*/\1/' | awk '{print $1 * 100}')
+
+# Check if Identity.Core branch coverage is found
+if [ -z "$IDENTITY_CORE_BRANCH_COVERAGE" ]; then
+  echo "No branch coverage data for Identity.Core found."
+  exit 1
+fi
 
 # Compare and exit accordingly
-if (( $(echo "$COVERAGE_PERCENTAGE < $THRESHOLD" | bc -l) )); then
-  echo "Code coverage is below $THRESHOLD%. It is $COVERAGE_PERCENTAGE%"
+if (( $(echo "$IDENTITY_CORE_BRANCH_COVERAGE < $THRESHOLD" | bc -l) )); then
+  echo "Identity.Core branch coverage is below $THRESHOLD%. It is $IDENTITY_CORE_BRANCH_COVERAGE%"
   exit 1
 else
-  echo "Code coverage is above $THRESHOLD%. It is $COVERAGE_PERCENTAGE%"
+  echo "Identity.Core branch coverage is above $THRESHOLD%. It is $IDENTITY_CORE_BRANCH_COVERAGE%"
 fi
