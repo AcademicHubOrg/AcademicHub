b414964f84ca68391bc3fa3384af48c9d1baae82
YaroslavKSE <ikhomych@kse.org.ua>
2023-12-09 14:32:33 +0200

    Fixed issues with addresses. Still need to integrate front and back in dockers

--- frontend/src/api/LocalHostAddresses.ts
+++ frontend/src/api/LocalHostAddresses.ts
@@ -1,7 +1,7 @@
 enum Addresses{
-    AUTH = "https://localhost:52999",
-    COURSESTREAMS = "https://localhost:52997",
-    COURSETEMPLATES = "https://localhost:5998",
-    MATERIALS = "https://localhost:52996"
+    AUTH = "http://localhost:52999",
+    COURSESTREAMS = "http://localhost:52997",
+    COURSETEMPLATES = "http://localhost:5998",
+    MATERIALS = "http://localhost:52996"
 }
 export {Addresses};
