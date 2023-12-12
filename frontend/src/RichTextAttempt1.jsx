b56909fb2cedf4893b556b529115d8ebe6ba9754
Zakhar-Khmurych <zakhar.khmurych@gmail.com>
2023-12-12 14:27:07 +0200

    idk if it works, i have no idea how and what should i do

---
+++ frontend/src/RichTextAttempt1.jsx
@@ -0,0 +1,23 @@
+﻿import React from 'react';
+import { HtmlToReact } from 'html-to-react';
+
+const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+);
+
+const updateTextDescription = async (state) => {
+    await setEditorState(state);
+    const data = convertToRaw(editorState.getCurrentContent());
+};
+
+
+<Editor
+    editorState={editorState}
+    toolbarClassName="toolbarClassName"
+    wrapperClassName="wrapperClassName"
+    editorClassName="editorClassName"
+    onEditorStateChange={updateTextDescription}
+/>
+
+
+setEditorState(EditorState.createWithContent(convertFromRaw(JSON.parse(current.description))));
