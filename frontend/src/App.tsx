0d11e51144a513fa63e9be9709af1eb30b1a98b1
YaroslavKSE <ikhomych@kse.org.ua>
2023-12-14 21:51:46 +0200

    Fixed linting error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
