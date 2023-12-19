026f5c0b03d2c9c23704a8873b311daf3abd3554
Maxim Pozdniakov <mpozdniakov@kse.org.ua>
2023-12-19 23:45:06 +0200

    connected frontend auth0 with backend

--- frontend/src/components/auth0Login.tsx
+++ frontend/src/components/auth0Login.tsx
@@ -1,10 +1,19 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
+import {loginService} from "../api/loginService";
+import React, {useEffect} from "react";
 
 const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithRedirect, user, isAuthenticated } = useAuth0();
+
+    useEffect(() => {
+        if (isAuthenticated && user && user.name && user.email) {
+            loginService(user.name, user.email)
+                .catch(error => console.error('POST request failed: ', error));
+        }
+    }, [isAuthenticated, user]);
 
     return <button onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
 export default LoginButton;
+
