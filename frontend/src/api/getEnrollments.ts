557a1751dc7458233579fa072d67105ebc92e49b
Maxim Pozdniakov <mpozdniakov@kse.org.ua>
2023-12-28 16:11:08 +0200

    courseService.ts logic changed to retrieve courseDetails using another endpoint

--- frontend/src/api/getEnrollments.ts
+++ frontend/src/api/getEnrollments.ts
@@ -1,5 +1,6 @@
 import { Addresses } from "./Addresses";
 import { getUserInfo } from "./getUserService";
+import {getCourseDetails} from "./courseService";
 
 interface User {
     id: string;
@@ -7,17 +8,23 @@ interface User {
     email: string;
 }
 
+interface Course {
+    id: string;
+    name: string;
+    templateId: string;
+}
+
 export const getEnrollments = async (studentEmail: string) => {
     const user: User = await getUserInfo(studentEmail);
     const userId = user.id;
     const response = await fetch(`${Addresses.COURSESTREAMS}/getEnrolledCourses/` + userId);
     const data = await response.json();
-    let courses = [];
+    let courses: Course[] = [];
 
     for (const enrollment of data.data) {
-        const courseResponse = await fetch(`${Addresses.COURSESTREAMS}/` + enrollment.courseStreamId);
-        const courseData = await courseResponse.json();
+        const courseData = await getCourseDetails(enrollment.courseStreamId);
         courses.push(courseData);
     }
     return courses;
 };
+
